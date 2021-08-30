4cefdf28faff645a945e6cb8be0551fed4d0af3b
ZeNoonan <darragh.noonan@gmail.com>
2021-08-30 21:17:09 +0100

    update

--- pga_stats.py
+++ pga_stats.py
@@ -8,13 +8,9 @@ import pickle
 
 st.set_page_config(layout="wide")
 
-combined=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/combined_1.pkl')
+combined=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/combined_2.pkl')
 
 
-
-
-# combined_1=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/results_riviera.pkl')
-# st.write('update',combined_1)
 
 def clean_results(file_location,name_of_tournament,date):
     df=pd.read_pickle(file_location)
@@ -28,164 +24,11 @@ def clean_results(file_location,name_of_tournament,date):
     df['Adj_Pos']=df['Adj_Pos'].fillna(df['Pos'])
     return df
 
-# q1=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8026',name_of_tournament='riviera',date=1)
-
-
-# q1=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8026',name_of_tournament='riviera',date=1)
-# q2=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8029',name_of_tournament='concession',date=2)
-# q3=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8033',name_of_tournament='bay_hill',date=3)
-# q4=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8038',name_of_tournament='sawgrass',date=4)
-# q5=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8045',name_of_tournament='honda',date=5)
-# q6=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8056',name_of_tournament='san_antonio',date=6)
-# q7=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8060',name_of_tournament='masters',date=7)
-# q8=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8064',name_of_tournament='harbour_town',date=8)
-# q9=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8083',name_of_tournament='innisbrook',date=9)
-# q10=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8091',name_of_tournament='quail_hollow',date=10)
-# q11=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8098',name_of_tournament='craig_ranch',date=11)
-# q12=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8102',name_of_tournament='kiawah_island',date=12)
-# q13=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8108',name_of_tournament='colonial',date=13)
-# q14=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8121',name_of_tournament='memorial',date=14)
-# q15=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8134',name_of_tournament='congaree',date=15)
-# q16=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8144',name_of_tournament='torrey_pines',date=16)
-# q17=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8155',name_of_tournament='river_highlands',date=17)
-# q18=clean_results(file_location='http://www.owgr.com/en/Events/EventResult.aspx?eventid=8170',name_of_tournament='detroit',date=18)
-# all_results=pd.concat([q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18])
-# all_results.to_csv('C:/Users/Darragh/Documents/Python/Golf/results_csv.csv')
-# st.write(all_results)
-
-# df_test=pd.read_csv('C:/Users/Darragh/Documents/Python/Golf/results_csv.csv')
-# df_test=df_test.rename(columns={'Name':'PLAYER NAME'})
-# st.write(df_test)
-
-# date_18 = pd.merge(combined_1, df_test,on=['PLAYER NAME','tournament','date'],how='outer')
-# date_18.to_csv('C:/Users/Darragh/Documents/Python/Golf/date_18.csv')
-
-
-
-
-
-
-
-
-
-
-
-
-
-# st.write('check combined', combined)
-
-# headers = {
-#     "accept": "application/json, text/javascript, */*; q=0.01",
-#     "accept-encoding": "gzip, deflate, br",
-#     "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-#     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.99 Safari/537.36",
-#     "x-requested-with": "XMLHttpRequest",
-# }
-# # https://stackoverflow.com/questions/64501788/api-web-data-capture
-# url = "https://www.pgatour.com/content/pgatour/stats/stat.02674.y2021.eon.t476.html"
-# # url = "https://www.pgatour.com/content/pgatour/stats/stat.02564.y2021.eon.t476.html"
-
-# df = pd.read_html(url, flavor="html5lib")
-# df = pd.concat(df).drop([0, 1, 2], axis=1)
-# st.write(df.head())
-
-
-# df.to_pickle('C:/Users/Darragh/Documents/Python/Golf/_02564_476_southwind.pkl')
-# df.to_pickle('C:/Users/Darragh/Documents/Python/Golf/_02674_476_southwind.pkl')
-
-# table=pd.read_html('http://www.owgr.com/en/Events/EventResult.aspx?eventid=9204')
-# table[0].to_pickle('C:/Users/Darragh/Documents/Python/Golf/results_southwind.pkl')
-# table=pd.read_html('http://www.owgr.com/en/Events/EventResult.aspx?eventid=179')
-# table[0].to_pickle('C:/Users/Darragh/Documents/Python/Golf/results_scottish_open.pkl')
-
-
-
-
-# RUN THIS FOR OGWR
-# results_ogwr=clean_results('C:/Users/Darragh/Documents/Python/Golf/results_southwind.pkl','southwind',23)
-# results_ogwr=results_ogwr.rename(columns={'Name':'PLAYER NAME'})
-# st.write(results_ogwr)
-# results_ogwr.to_pickle('C:/Users/Darragh/Documents/Python/Golf/results_southwind_ogwr.pkl')
-# st.write('twin cities ogwr results', results_twin_cities)
-# results_ogwr=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/results_southwind_ogwr.pkl')
-# results_ogwr.loc [ (results_ogwr['PLAYER NAME']=='Cameron Davis'), 'PLAYER NAME' ] = 'Cam Davis'
-# results_ogwr.loc [ (results_ogwr['PLAYER NAME']=='Siwoo Kim'), 'PLAYER NAME' ] = 'Si Woo Kim'
-# results_ogwr.loc [ (results_ogwr['PLAYER NAME']=='Kyoung-Hoon Lee'), 'PLAYER NAME' ] = 'K.H. Lee'
-
-
-# st.write('change player names', results_ogwr[results_ogwr['PLAYER NAME'].str.contains('avis')])
-# st.write('change player names', results_ogwr[results_ogwr['PLAYER NAME'].str.contains('Siwoo')])
-# st.write('change player names', results_ogwr[results_ogwr['PLAYER NAME'].str.contains('Kyou')])
-
-# x=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/twin_cities_stats.pkl')
-# st.write(x)
-
-# test=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/olympics_stats.pkl')
-# st.write('stats??', test)
-
-
-# def stats_results(event_ogwr=results_ogwr,stats_pickle='C:/Users/Darragh/Documents/Python/Golf/southwind_stats.pkl'):
-#     # event_ogwr=pd.read_pickle(ogwr_pickle)
-#     event_ogwr['Adj_Pos']=event_ogwr['Pos'].str.replace('WD','')
-#     event_stats=pd.read_pickle(stats_pickle)
-#     return pd.merge(event_stats, event_ogwr,on=['PLAYER NAME','tournament','date'],how='outer')
-
-# result_stat_event=stats_results(event_ogwr=results_ogwr)
-# result_stat_event.to_pickle('C:/Users/Darragh/Documents/Python/Golf/southwind_stats_results.pkl')
-# result_stat_event=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/southwind_stats_results.pkl')
-# st.write('check this stats/results of event', result_stat_event.head())
-# st.write('check nan',result_stat_event[(result_stat_event['Pos'].isnull()) ])
-
-
-
-# def combine_db(df, event_stats_results):
-#     return pd.concat([df,event_stats_results],ignore_index=True)
-
-# combine_db=combine_db(combined,result_stat_event)
-# st.write('checking merge of event of stats and db', combine_db)
-
-# combine_db.to_pickle('C:/Users/Darragh/Documents/Python/Golf/combined_1.pkl')
-
-# results_deere_run=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/results_deere_run.pkl')
-
-# results_deere_run=results_deere_run.rename(columns={'Name':'PLAYER NAME'})
-# st.write('deere run', results_deere_run.head())
-# my_updated_data = pd.merge(combined, results_deere_run,on=['PLAYER NAME','tournament','date'],how='outer')
-# my_updated_data.to_pickle('C:/Users/Darragh/Documents/Python/Golf/data_stats_results_2.pkl')
-
-
-# MIGHT NEED BELOW
-# my_data=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/data_stats_results_1.pkl')
-# st.write('my data', my_data.tail())
-# combined_results=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/results_combined.pkl')
-# combined_results=combined_results.rename(columns={'Name':'PLAYER NAME'})
-# my_updated_data = pd.merge(my_data, results_deere_run,on=['PLAYER NAME','tournament','date','Pos','Ctry','R1','R2','R3','R4','Ranking Points','Adj_Pos','Agg'],how='outer')
-# st.write('after merge', my_updated_data.tail())
-# my_updated_data.to_pickle('C:/Users/Darragh/Documents/Python/Golf/merged_data.pkl')
-
-# combined=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/data_stats_results.pkl')
-# st.write('data stats results',combined.tail())
-# test_1=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/data_stats_results_1.pkl')
-# st.write('data stats results 1',test_1.tail())
-
-
-# southwind=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/_02674_476_southwind.pkl')
-# putt_southwind=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/_02564_476_southwind.pkl')
-
-
-
-
-
 
 british_open=clean_results('C:/Users/Darragh/Documents/Python/Golf/results_british_open.pkl','british_open',20)
 british_open=british_open.rename(columns={'Name':'PLAYER NAME'})
 combined['Tee_App_Rank']=combined['Tee_App_Rank'].replace(np.NaN,75)
 combined=combined.sort_values(by=['PLAYER NAME', 'date'])
-
-# combined['Adj_Pos']=combined['Adj_Pos'].str.replace('T','')
-# combined['Adj_Pos']=pd.to_numeric(combined['Adj_Pos'],errors='coerce')
-
-
 
 
 def test_4(df):
@@ -208,16 +51,6 @@ combined=df[cols]
 # st.write(combined.tail())
 combined = pd.merge(combined, british_open,on=['PLAYER NAME','tournament','date','Pos','Ctry','R1','R2','R3','R4','Ranking Points','Adj_Pos','Agg'],how='outer')
 # st.write('check to see if no.21 is in here keene trace', combined)
-
-def merge_golf(tee_to_green_stats,putting_stats, name_of_tournament,date):
-    tee_to_green_stats = tee_to_green_stats.loc[:,['MEASURED ROUNDS','PLAYER NAME','ROUNDS','SG:APR','SG:ARG','SG:OTT']].copy()
-    putting_stats = putting_stats.loc[:,['MEASURED ROUNDS','PLAYER NAME','ROUNDS','TOTAL SG:PUTTING']].copy()
-    df = pd.merge(tee_to_green_stats, putting_stats, on=['PLAYER NAME', 'ROUNDS', 'MEASURED ROUNDS'])
-    df['tournament']=name_of_tournament
-    df['date']=date
-    df=df.iloc[1:].copy()
-    return df
-
 
 
 def clean_golf_tee(df):
@@ -279,15 +112,7 @@ format_dict = {'TOTAL SG:OTT':'{0:,.0f}','SG:OTT':'{0:,.0f}', 'SG:APR':'{0:,.0f}
 'PUTT_Rank':'{0:,.0f}','SG_OTT_Avg_Rank':'{0:,.0f}','Tee_App_Rank':'{0:,.0f}','ARG_Rank':'{0:,.0f}','TOTAL SG:TEE:ARG':'{0:,.0f}','SG_Rank':'{0:,.0f}',
 'SG :Tee_Arg_AVG':'{0:,.1f}','Tee_Arg_Rank':'{0:,.0f}','SG_Rank_less_Rank':'{0:,.0f}','TOTAL SG:TEE_APR':'{0:,.0f}','TOTAL SG:TEE_ARG':'{0:,.0f}','Appr_Rank':'{0:,.0f}'  }
 
-# event_stats=merge_golf(southwind, putt_southwind,'southwind',23)
-# event_stats=clean_golf_tee(event_stats).reset_index()
-# event_stats.to_pickle('C:/Users/Darragh/Documents/Python/Golf/southwind_stats.pkl')
-# combined=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/data_stats_results.pkl')
-# combined = pd.concat([combined,deere_run_stats])
 
-
-# combined = combined.reset_index()
-# combined.to_pickle('C:/Users/Darragh/Documents/Python/Golf/combined_stats.pkl')
 
 
 # st.write('this is combined database of the tournaments sorted by SG: TOTAL_AVG')
@@ -372,5 +197,68 @@ with st.beta_expander('Database grouped by Player over all tournaments'):
 
 # st.write('check nan',combined[(combined['Pos'].isnull()) & (combined['MEASURED ROUNDS']>3) ])
 
-with st.beta_expander('Notes'):
+with st.beta_expander('Weekly Scrape'):
     st.write('when adding a major with no SG values, be careful, add this data in after the calculations are done')
+
+    # headers = {
+    #     "accept": "application/json, text/javascript, */*; q=0.01",
+    #     "accept-encoding": "gzip, deflate, br",
+    #     "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+    #     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.99 Safari/537.36",
+    #     "x-requested-with": "XMLHttpRequest",
+    # }
+    # # https://stackoverflow.com/questions/64501788/api-web-data-capture
+    # # url = "https://www.pgatour.com/content/pgatour/stats/stat.02674.y2021.eon.t013.html"
+    # url = "https://www.pgatour.com/content/pgatour/stats/stat.02564.y2021.eon.t013.html"
+
+    # df = pd.read_html(url, flavor="html5lib")
+    # df = pd.concat(df).drop([0, 1, 2], axis=1)
+    # st.write(df.head())
+
+
+    # df.to_pickle('C:/Users/Darragh/Documents/Python/Golf/_02564_013_sedgefield.pkl')
+    # # df.to_pickle('C:/Users/Darragh/Documents/Python/Golf/_02674_013_sedgefield.pkl')
+    
+    # putt=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/_02564_013_sedgefield.pkl')
+    # sg=pd.read_pickle('C:/Users/Darragh/Documents/Python/Golf/_02674_013_sedgefield.pkl')
+    # # st.write('sg',sg)
+    # # st.write('putt', putt)
+
+    # def merge_golf(tee_to_green_stats,putting_stats, name_of_tournament,date):
+    #     tee_to_green_stats = tee_to_green_stats.loc[:,['MEASURED ROUNDS','PLAYER NAME','ROUNDS','SG:APR','SG:ARG','SG:OTT']].copy()
+    #     putting_stats = putting_stats.loc[:,['MEASURED ROUNDS','PLAYER NAME','ROUNDS','TOTAL SG:PUTTING']].copy()
+    #     df = pd.merge(tee_to_green_stats, putting_stats, on=['PLAYER NAME', 'ROUNDS', 'MEASURED ROUNDS'])
+    #     df['tournament']=name_of_tournament
+    #     df['date']=date
+    #     df=df.iloc[1:].copy()
+    #     return df
+
+    # event_stats=merge_golf(sg, putt,'sedgefield',24)
+    # event_stats=clean_golf_tee(event_stats).reset_index()
+    # event_stats.to_pickle('C:/Users/Darragh/Documents/Python/Golf/sedgefield_stats.pkl')
+
+    # table=pd.read_html('http://www.owgr.com/en/Events/EventResult.aspx?eventid=9214')
+    # table[0].to_pickle('C:/Users/Darragh/Documents/Python/Golf/results_sedgefield.pkl')
+    # results_ogwr=clean_results('C:/Users/Darragh/Documents/Python/Golf/results_sedgefield.pkl','sedgefield',24)
+    # results_ogwr=results_ogwr.rename(columns={'Name':'PLAYER NAME'})
+    # results_ogwr.loc [ (results_ogwr['PLAYER NAME']=='Cameron Davis'), 'PLAYER NAME' ] = 'Cam Davis'
+    # results_ogwr.loc [ (results_ogwr['PLAYER NAME']=='Siwoo Kim'), 'PLAYER NAME' ] = 'Si Woo Kim'
+    # results_ogwr.loc [ (results_ogwr['PLAYER NAME']=='Kyoung-Hoon Lee'), 'PLAYER NAME' ] = 'K.H. Lee'
+    # results_ogwr.loc [ (results_ogwr['PLAYER NAME']=='Sunghoon Kang'), 'PLAYER NAME' ] = 'Sung Kang'
+    # results_ogwr.loc [ (results_ogwr['PLAYER NAME']=='Sebastian Munoz'), 'PLAYER NAME' ] = 'Sebastián Muñoz'
+    # results_ogwr.loc [ (results_ogwr['PLAYER NAME']=='Benjamin Taylor'), 'PLAYER NAME' ] = 'Ben Taylor'
+
+
+
+    # result_stat_event= pd.merge(event_stats, results_ogwr,on=['PLAYER NAME','tournament','date'],how='outer')
+    # st.write('check nan',result_stat_event[(result_stat_event['Pos'].isnull()) ])
+    # # st.write(result_stat_event)
+
+    # def combine_db(df, event_stats_results):
+    #     return pd.concat([df,event_stats_results],ignore_index=True)
+
+    # combine_db=combine_db(combined,result_stat_event)
+    # combine_db.to_pickle('C:/Users/Darragh/Documents/Python/Golf/combined_2.pkl')
+
+
+
