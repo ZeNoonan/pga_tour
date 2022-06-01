2260e808b90f0a562a023fdc6dccacdb59d5c253
ZeNoonan <darragh.noonan@gmail.com>
2022-06-01 18:31:27 +0100

    update

--- golf_rankings.py
+++ golf_rankings.py
@@ -1,5 +1,3 @@
-from math import comb
-from tkinter import CENTER
 import streamlit as st
 import pandas as pd
 import numpy as np
--- golf_rankings_1.py
+++ golf_rankings_1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import pathlib
 from st_aggrid import AgGrid, GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
 st.set_page_config(layout="wide")
@@ -28,6 +29,14 @@ def clean_results_matchplay(file_location,name_of_tournament,year):
     # df["R1"]=pd.to_numeric(df["R1"],errors='coerce')
     return df
 
+def ogwr_file_csv_save(url_comp,filename_ext):
+    table=pd.read_html(url_comp)
+    p=pathlib.Path.cwd().joinpath('golf','rankings_data')
+    return table[0].to_csv(p.joinpath(filename_ext))
+
+# ogwr_file_csv_save('http://www.owgr.com/en/Events/EventResult.aspx?eventid=9522','colonial.csv')
+
+
 masters=clean_results('C:/Users/Darragh/Documents/Python/Golf/rankings_data/masters_2022.csv','masters tournament',2022)
 players=clean_results('C:/Users/Darragh/Documents/Python/Golf/rankings_data/players.csv','the players championship',2022)
 matchplay=clean_results_matchplay('C:/Users/Darragh/Documents/Python/Golf/rankings_data/matchplay.csv','wgc - dell technologies match play',2022)
@@ -50,11 +59,14 @@ craig_ranch_texas=clean_results('C:/Users/Darragh/Documents/Python/Golf/rankings
 vidanta_mexico=clean_results('C:/Users/Darragh/Documents/Python/Golf/rankings_data/vidanta_mexico.csv','mexico open at vidanta',2022)
 pebble_beach=clean_results('C:/Users/Darragh/Documents/Python/Golf/rankings_data/pebble_beach.csv','at&t pebble beach pro-am',2022)
 uspga=clean_results('C:/Users/Darragh/Documents/Python/Golf/rankings_data/uspga.csv','u.s. pga championship',2022)
+colonial=clean_results('C:/Users/Darragh/Documents/Python/Golf/rankings_data/colonial.csv','charles schwab challenge',2022)
 
 tournament_list=[masters,players, matchplay, riviera, bay_hill, scottsdale, kapalua, torrey_pines,innisbrook, jeddah, la_quinta,
-dubai,hawaii,abu_dhabi,palm_beach,hilton_head, san_antonio, potomac,craig_ranch_texas,vidanta_mexico,pebble_beach, uspga]
+dubai,hawaii,abu_dhabi,palm_beach,hilton_head, san_antonio, potomac,craig_ranch_texas,vidanta_mexico,pebble_beach, uspga,colonial]
 combined=pd.concat(tournament_list,axis=0)
 
+# events=pd.read_html('http://www.owgr.com/events')
+# events[0].to_csv('C:/Users/Darragh/Documents/Python/Golf/rankings_data/ranking_events.csv')
 ranking_events=pd.read_csv('C:/Users/Darragh/Documents/Python/Golf/rankings_data/ranking_events.csv')
 ranking_events['World Rating']=pd.to_numeric(ranking_events['World Rating'],errors='coerce')
 ranking_events['Event Name']=ranking_events['Event Name'].str.lower()
@@ -65,13 +77,31 @@ combined=combined.sort_values(['Name','Week'],ascending=True)
 
 combined['events_count']=combined.groupby(['Name'])['Points Won'].cumcount()+1
 combined['max_event']=combined.groupby('Name')['events_count'].transform('max')
+combined['rolling_4_pts_count']=combined.groupby('Name')['Points Won'].rolling(4, min_periods=1).count().reset_index(0,drop=True).astype(int)
 combined['rolling_4_pts_total']=combined.groupby('Name')['Points Won'].rolling(4, min_periods=1).sum().reset_index(0,drop=True)
 combined['rolling_4_pts_mean']=combined.groupby('Name')['Points Won'].rolling(4, min_periods=1).mean().reset_index(0,drop=True)
 combined['rolling_4_pts_median']=combined.groupby('Name')['Points Won'].rolling(4, min_periods=1).median().reset_index(0,drop=True)
+combined['rolling_8_pts_count']=combined.groupby('Name')['Points Won'].rolling(8, min_periods=1).count().reset_index(0,drop=True).astype(int)
 combined['rolling_8_pts_total']=combined.groupby('Name')['Points Won'].rolling(8, min_periods=1).sum().reset_index(0,drop=True)
 combined['rolling_8_pts_mean']=combined.groupby('Name')['Points Won'].rolling(8, min_periods=1).mean().reset_index(0,drop=True)
 combined['rolling_8_pts_median']=combined.groupby('Name')['Points Won'].rolling(8, min_periods=1).median().reset_index(0,drop=True)
 
 
 # https://stackoverflow.com/questions/13996302/python-rolling-functions-for-groupby-object
-st.write(combined)
+st.write(combined[combined['Name'].str.contains("Scheff")])
+
+with st.expander('Last 8 events'):
+    number_of_events=st.number_input(label='number of events for points won',min_value=2,key='number_weeks',value=8)
+    week_number=st.number_input(label='select week number',min_value=2,key='week_number',value=20)
+    selected_df=combined[combined['Week']<(week_number+1)].groupby('Name').head(number_of_events).reset_index(0,drop=True)
+    
+    combined['rolling_pts_count']=combined.groupby('Name')['Points Won'].rolling(number_of_events, min_periods=1).count().reset_index(0,drop=True).astype(int)
+    combined['rolling_pts_total']=combined.groupby('Name')['Points Won'].rolling(number_of_events, min_periods=1).sum().reset_index(0,drop=True)
+    combined['rolling_pts_mean']=combined.groupby('Name')['Points Won'].rolling(number_of_events, min_periods=1).mean().reset_index(0,drop=True)
+    combined['rolling_pts_median']=combined.groupby('Name')['Points Won'].rolling(number_of_events, min_periods=1).median().reset_index(0,drop=True)
+
+    st.write(selected_df[selected_df['Name'].str.contains("Scheff")])
+
+    selected_df['avg_pts_rank']=selected_df['rolling_pts_mean'].rank(method='dense', ascending=False).astype(int)
+    selected_df['median_pts_rank']=selected_df['rolling_pts_median'].rank(method='dense', ascending=False).astype(int)
+    selected_df['avg_plus_med_rank']=(selected_df['avg_pts_rank']+selected_df['median_pts_rank']).rank(method='dense', ascending=True).astype(int)
